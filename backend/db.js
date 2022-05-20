7940b9be40454b33728491536b61751581377732
andi bev <andi.bevapi@gmail.com>
2022-05-20 17:28:00 +0200

    backend schemas

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const URI = "mongodb+srv://andi:andi@cluster0.upb5e.gcp.mongodb.net/?retryWrites=true&w=majority";
+
+const connectDB = async() =>{
+    try{
+        const connect = await mongoose.connect(URI);
+        console.log("connected-----".bgYellow);
+    }catch(error){
+        console.log("error-----",error);
+    }
+}
+
+module.exports = connectDB;
