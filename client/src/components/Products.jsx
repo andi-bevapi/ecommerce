6dd918e6439bdb6404de092e22d920eabddb6fb0
andi bev <andi.bevapi@gmail.com>
2022-11-23 16:39:27 +0100

    frontend

---
+++ client/src/components/Products.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import Product from "./Product";
+import axios from "axios";
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Products = ({ cat, filters, sort }) => {
+  
+  console.log("filters----",filters);
+
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+
+
+  return (
+    <Container>
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
+    </Container>
+  );
+};
+
+export default Products;
